083b3c345a63cc3471684e599cdafba7f743da27
Vitor Henrique <vhsouzaeng@gmail.com>
2022-05-25 22:19:50 -0300

    feat: add roboto font as default

---
+++ apps/front/src/presentation/styles/Global.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components"
+
+const globalStyle = createGlobalStyle`
+    @font-face {
+        font-family: roboto;
+        src: url("/fonts/Roboto-Black.ttf");
+        font-weight: 900;
+    }
+
+    @font-face {
+        font-family: roboto;
+        src: url("/fonts/Roboto-Bold.ttf");
+        font-weight: 700;
+    }
+
+    @font-face {
+        font-family: roboto;
+        src: url("/fonts/Roboto-Medium.ttf");
+        font-weight: 500;
+    }
+    
+    *{
+        font-family: Roboto, Arial, Helvetica, sans-serif;
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+`
+
+export default globalStyle
