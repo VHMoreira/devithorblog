573468d2fdef417d2047fd9e8eb5904690e2c3c6
Vitor Henrique <vhsouzaeng@gmail.com>
2022-05-26 22:19:05 -0300

    feat: create social media icons

--- apps/front/src/presentation/components/ui/Icons/index.ts
+++ apps/front/src/presentation/components/ui/Icons/index.ts
@@ -1,1 +1,5 @@
 export { default as LogoWhite } from "./LogoWhite";
+export { default as GithubIcon } from "./GithubIcon";
+export { default as InstagramIcon } from "./InstagramIcon";
+export { default as LinkedinIcon } from "./LinkedinIcon";
+export { default as TwitterIcon } from "./TwitterIcon";
